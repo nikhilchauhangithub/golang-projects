9b0abaa28c49fa8d07f4f4605e9b642d81410569
Nikhil <thisisnikhilchauhan@gmail.com>
2023-04-19 00:48:21 +0530

    get and delete func

--- 02Go-Movies-Crud/main.go
+++ 02Go-Movies-Crud/main.go
@@ -28,6 +28,18 @@ func getMovies(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(movies)
 }
 
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params:=mux.Vars(r)
+	for index,item:=range movies {
+		if item.ID ==params["id"]{
+			movies= append(movies[:index], movies[index+1:]...)
+		break
+		}
+		
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 	movies = append(movies, Movie{ID: "1",Isbn: "48265",Title: "Movie One",Director: &Director{Firstname: "John", Lastname: "Doe"}})
